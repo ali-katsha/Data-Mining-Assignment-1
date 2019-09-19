11dcbd4acc6c4023767f1e93f6ab1372843af03c
Ali Katsheh <alikatsheh@Alis-MacBook-Pro.local>
2019-09-19 16:09:39 +0200

    read_files

--- ClassificationTree.R
+++ ClassificationTree.R
@@ -1,3 +1,15 @@
-d<-read.table("credit.txt",header=TRUE,sep=" ")
-d[2,]
-d
+read_file <-function(){
+  
+  d<-read.table("credit.txt",header=TRUE,sep=",")
+  m <- as.matrix(d)
+  return(m);
+}
+
+
+
+
+
+
+
+
+data = read_file()
